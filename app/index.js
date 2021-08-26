03d5f7a2c096d8ff4b529a790cfd0b85bbe8170e
Andzie-M347 <andzisishine@gmail.com>
2021-08-26 08:08:15 +0200

    Fetch Images

--- app/index.js
+++ app/index.js
@@ -1,5 +1,37 @@
+import {
+    createApi
+} from "unsplash-js";
 import './css/main.css';
 
-console.log(process.env.UNSPLASH_ACCESS_KEY);
+const api = createApi({
+    accessKey: process.env.UNSPLASH_ACCESS_KEY
+});
 
-console.log('hello, world');
+// Get image container
+
+const imageContainer = document.querySelector('.photohub-content');
+
+
+const url = 'https://api.unsplash.com/photos?page=1'
+
+
+api.photos.list(url).then(result => {
+  if (result.type === 'success') {
+      const photos = result.response;
+      
+      photos.results.map((photo) => {
+          const image = document.createElement('figure');
+          image.classList.add('img-reponsive');
+          image.innerHTML = `<img src="${photo.urls.thumb}"/>`;
+
+          imageContainer.appendChild(image);
+      })
+    // unsplash.photos.trackDownload({
+    //   downloadLocation: photo.links.download_location,
+    // });
+  }
+});
+
+
+
+
