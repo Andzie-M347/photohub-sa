9be258860e67fd6a6c6c6930586f54f821f4bc64
Andzie-M347 <andzisishine@gmail.com>
2021-08-26 09:54:09 +0200

    Added basic styles!

--- app/index.js
+++ app/index.js
@@ -9,29 +9,32 @@ const api = createApi({
 
 // Get image container
 
-const imageContainer = document.querySelector('.photohub-content');
-
-
-const url = 'https://api.unsplash.com/photos?page=1'
+const imageContainer = document.querySelector('.photohub-content__wrapper');
+const url = 'https://api.unsplash.com/photos'
 
 
 api.photos.list(url).then(result => {
-  if (result.type === 'success') {
-      const photos = result.response;
-      
-      photos.results.map((photo) => {
-          const image = document.createElement('figure');
-          image.classList.add('img-reponsive');
-          image.innerHTML = `<img src="${photo.urls.thumb}"/>`;
+    if (result.type === 'success') {
+        const photos = result.response;
+        console.log(photos);
 
-          imageContainer.appendChild(image);
-      })
-    // unsplash.photos.trackDownload({
-    //   downloadLocation: photo.links.download_location,
-    // });
-  }
+        photos.results.map((photo) => {
+            const figure = document.createElement('figure');
+            figure.classList.add('photohub-content__image');
+            figure.innerHTML = `<img src="${photo.urls.raw}" class="img-responsive"/>`;
+            imageContainer.appendChild(figure);
+            const figcaption = document.createElement('figcaption');
+            figcaption.innerHTML = `<div class="cite">
+                    <img src=${photo.user.profile_image.small} class="cite__rounded" alt=${photo.alt_description}>
+                     <a href="javascript:void(0)" class="cite__title" title=${photo.alt_description}> ${photo.user.first_name}</a>
+                </div>`;
+            figure.appendChild(figcaption);
+        })
+        // unsplash.photos.trackDownload({
+        //   downloadLocation: photo.links.download_location,
+        // });
+    }
 });
 
 
-
-
+{/* <small> ${photo.sponsorship.sponser.tagline == 'undefined' ? photo.sponsorship.sponser.tagline : '' } </small> */}
