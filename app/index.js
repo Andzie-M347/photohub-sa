985a7ae71ce554bce2c80ea94698998f127b2fad
Andzie-M347 <andzisishine@gmail.com>
2021-09-09 03:50:39 +0200

    Infinite scroll loader

--- app/index.js
+++ app/index.js
@@ -11,6 +11,76 @@ const api = createApi({
   accessKey: process.env.UNSPLASH_ACCESS_KEY
 });
 
+const loader = document.querySelector('.loader');
+const apiKey = '783f46460ebede7f21f34b84eb80206e27d042af75812821ffbcd17828afee3f'
+const count = 10;
+let page = 0;
+const url = 'https://api.unsplash.com';
+const apiEndpoint = `${url}/photos/?client_id=${apiKey}&page=${page}&per_page=${count}` ;
+
+
+
+
+// async function getResponse() {
+//   const pro = await fetch(apiEndpoint);
+//   return pro;
+// }
+
+// getResponse();
+
+// Get image container
+const imageContainer = document.querySelector('.photohub-content__wrapper');
+
+const loadMore = async () => {
+  loader.style.display = "block";
+
+    // console.log(`${url}/photos/?client_id=${apiKey}&page=${page++}&per_page=${count}`);
+    await fetch(`${url}/photos/?client_id=${apiKey}&page=${page++}&per_page=${count}`)
+  
+    .then(res => res.json())
+    .then(data => {
+      if (data.length > 0) {
+
+        data.map(photo => {
+          const figure = document.createElement('figure');
+          figure.classList.add('photohub-content__image');
+          figure.innerHTML = `
+                <a href="javascript:void(0)" >
+                    <img src="${photo.urls.thumb}" class="img-responsive" title="${photo.alt_description}" />
+                </a>
+            `;
+          imageContainer.appendChild(figure);
+          const figcaption = document.createElement('figcaption');
+          figcaption.innerHTML = `<div class="cite">
+                    <img src=${photo.user.profile_image.small} class="cite__rounded" alt="${photo.alt_description}">
+                     <a href="javascript:void(0)" class="cite__title" title="${photo.user.first_name}"> ${photo.user.first_name}</a>
+                </div>`;
+          figure.appendChild(figcaption);
+
+          console.log(photo);
+        })
+      }
+
+
+      loader.style.display = "none";
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+
+window.addEventListener('scroll', function () {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) {
+    
+    loadMore();
+  }
+});
+
+loadMore();
+
+
 // Dropdown menu
 (function () {
   var elements = document.querySelectorAll(".dropdown-button");
@@ -58,7 +128,7 @@ const search = document.querySelector('#search');
 search.addEventListener('input', () => searchTopics(search.value));
 
 const searchTopics = async searchText => {
-  const res = await fetch(`https://api.unsplash.com/topics/?per_page=30&client_id=${process.env.UNSPLASH_ACCESS_KEY}`)
+  const res = await fetch(`${url}/topics/?per_page=30&client_id=${process.env.UNSPLASH_ACCESS_KEY}`)
   const topics = await res.json();
 
   let matches = topics.filter(topic => {
@@ -70,7 +140,7 @@ const searchTopics = async searchText => {
     matches = []
     output.innerHTML = '';
     output.classList.remove('output-card__inner');
-    
+
   }
 
   outputHtml(matches);
@@ -79,7 +149,7 @@ const searchTopics = async searchText => {
 
 const outputHtml = (matches) => {
   if (matches.length > 0) {
-    
+
     output.classList.add('output-card__inner');
     const htmlOutput = matches.map((match) => {
       return `<div class="search-results" > 
@@ -89,69 +159,39 @@ const outputHtml = (matches) => {
 
 
     output.innerHTML = htmlOutput;
-  } 
+  }
 }
 
 
 
-
-
-
-
-
 // Get single Image 
-const banner = document.querySelector('.showcase-banner');
+// const banner = document.querySelector('.showcase-banner');
 
-api.photos.getRandom('https://api.unsplash.com/photos/photos/random', {
-  collectionIds: ['abc123'],
-  topicIds: ['def456'],
-  featured: true,
-  username: 'naoufal',
-  query: 'dog',
-  count: 1,
-}).then(result => {
-  const photo = result.response;
-  banner.style.background = `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${photo.urls.regular}) 0% 0% / cover no-repeat`;
-  const cite = document.createElement('div');
-  cite.classList.add('showcase-banner__cite');
-  cite.innerHTML = `<div class="cite-box">
-         <small class="box-cite__user">
-            <a href="javascript:void(0)">
-                <span> Photo by ${photo.user.first_name} </span
-            </a>
-         </small>
-    </div>`;
-  banner.appendChild(cite);
-});
+// api.photos.getRandom('https://api.unsplash.com/photos/photos/random', {
+//   collectionIds: ['abc123'],
+//   topicIds: ['def456'],
+//   featured: true,
+//   username: 'naoufal',
+//   query: 'dog',
+//   count: 1,
+// }).then(result => {
+//   const photo = result.response;
+//   banner.style.background = `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${photo.urls.regular}) 0% 0% / cover no-repeat`;
+//   const cite = document.createElement('div');
+//   cite.classList.add('showcase-banner__cite');
+//   cite.innerHTML = `<div class="cite-box">
+//          <small class="box-cite__user">
+//             <a href="javascript:void(0)">
+//                 <span> Photo by ${photo.user.first_name} </span
+//             </a>
+//          </small>
+//     </div>`;
+//   banner.appendChild(cite);
+// });
 
 
-// Get image container
-const imageContainer = document.querySelector('.photohub-content__wrapper');
 
-api.photos.list('https://api.unsplash.com/photos').then(result => {
-  if (result.type === 'success') {
-    const photos = result.response;
-    photos.results.map((photo) => {
-      const figure = document.createElement('figure');
-      figure.classList.add('photohub-content__image');
-      figure.innerHTML = `
-                <a href="javascript:void(0)" >
-                    <img src="${photo.urls.thumb}" class="img-responsive" title="${photo.alt_description}" />
-                </a>
-            `;
-      imageContainer.appendChild(figure);
-      const figcaption = document.createElement('figcaption');
-      figcaption.innerHTML = `<div class="cite">
-                    <img src=${photo.user.profile_image.small} class="cite__rounded" alt="${photo.alt_description}">
-                     <a href="javascript:void(0)" class="cite__title" title="${photo.user.first_name}"> ${photo.user.first_name}</a>
-                </div>`;
-      figure.appendChild(figcaption);
-    })
-    // unsplash.photos.trackDownload({
-    //   downloadLocation: photo.links.download_location,
-    // });
-  }
-});
+
 
 // Tags - topics
 const tags = document.querySelector('.topics');
@@ -172,3 +212,19 @@ api.topics.list('https://api.unsplash.com/topics', {
   });
 
 })
+
+
+// Load more button
+
+const more = document.querySelector('#load-more');
+
+more.addEventListener('click', () => {
+  page++;
+
+  if (fetchLink) {
+    `${url}/photos/?per_page=12&page=${page}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+  }
+
+  console.log(fetchLink);
+
+});
